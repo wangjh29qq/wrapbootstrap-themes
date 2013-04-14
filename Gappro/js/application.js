ff2386f0b0b26bd2bdeac4b943c745a63e052778
bob <1982wb@gmail.com>
2013-04-15 00:18:40 +0800

    搞定back to top按钮效果和脚本

--- Gappro/js/application.js
+++ Gappro/js/application.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
 		});
 	});
 
+	// guide tour
 	$("#tour").css('display', 'none');
 	$("#btn-tour, #btn-tour-take").bind("click", function(e) {
 		$("#tour").joyride({
@@ -44,6 +45,24 @@ $(document).ready(function() {
 			'tipLocation': 'top'
 		});
 		e.preventDefault();
+	});
+
+	//top
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#back-top').fadeIn();
+		} else {
+			$('#back-top').fadeOut();
+		}
+	});
+
+	// scroll body to 0px on click
+	$('#back-top a').click(function () {
+		$('body, html').animate({
+			scrollTop: 0
+		}, 800);
+
+		return false;
 	});
 });
 
