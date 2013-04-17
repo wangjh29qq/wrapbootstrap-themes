7fcdccfd5cc93123211aa22f35a0fbbad51ec6ac
bob <1982wb@gmail.com>
2013-04-17 15:51:11 +0800

    fix widget config&help box

--- Gappro/js/gappro/gappro.js
+++ Gappro/js/gappro/gappro.js
@@ -146,14 +146,14 @@ $(".widget .widget-header > div > a[data-rel='widget']").each(function() {
 		case 'config':
 			$(this).html('<i class="icon-cog"></i>');
 			$(this).bind('click', function() {
-				$("#" + $(this).attr("target-box")).modal();
+				$("#" + $(this).attr("data-box")).modal();
 			});
 	        break;
 
 		case 'help':
 			$(this).html('<i class="icon-question-sign"></i>');
 			$(this).bind('click', function() {
-				$("#" + $(this).attr("target-box")).modal();
+				$("#" + $(this).attr("data-box")).modal();
 			});
 	        break;
 	        
